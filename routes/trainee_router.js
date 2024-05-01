68e52fda098b150ef385d4d549cd31c1e7468a99
omar-mostafa-taha <omar195628@feng.bu.edu.eg>
2024-05-01 23:40:11 +0300

    router modified - fixed the import issue

--- routes/trainee_router.js
+++ routes/trainee_router.js
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import * as trainee_controller from "../controllers/Trainee_controller.js";
+const { Router } = require("express");
 
 const traineeRouter = Router();
+const trainee_controller = require("../controllers/Trainee_controller.js")
+
 traineeRouter.get("/", trainee_controller.getAlltrainee);
 traineeRouter.get("/:id", trainee_controller.getraineeById);
 traineeRouter.post("/", trainee_controller.addtrainee);
 traineeRouter.put("/:id", trainee_controller.edittraineeById);
 traineeRouter.delete("/:id", trainee_controller.deletetrainee);
 
-export { traineeRouter };
-
+module.exports = { traineeRouter };
