d2168c75e9a5c85478c8d8d4b4b3494d98a8191e
omar-mostafa-taha <omar195628@feng.bu.edu.eg>
2024-05-01 23:29:46 +0300

    use trainee router

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const {trainerRouter} = require("./routes/trainer_router.js");
+const { trainerRouter } = require("./routes/trainer_router.js");
+const { traineeRouter } = require("./routes/trainee_router.js");
+
 const express = require("express");
 const app = express();
 app.use(express.json());
 
 
 
-// app.use("/api/trainees/", traineeRouter);
+app.use("/api/trainees/", traineeRouter);
 app.use("/api/trainers/", trainerRouter);
 
-module.exports =  { app };
+module.exports = { app };
